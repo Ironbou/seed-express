769564a1e8869f9f36a18d6f55b46b12b5be2800
ironbou <jojo-munoz1@hotmail.fr>
2016-12-08 14:59:20 +0100

    Initialisation du dépot.

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var path = require ('path');
+
+var app = express();
+
+app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile);
+
+app.get('*', function(req, res, next) {
+  res.render('../Dist/riri.html');
+});
+
+app.set('port', 80);
+var server = app.listen(app.get('port'), function() {
+  var port = server.address().port;
+  console.log('listen port:80');
+});
